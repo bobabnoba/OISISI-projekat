6ee215b6e235025013c674434e9bd2957f64c3f0
markovic-dusan <shandusukisuki3@gmail.com>
2020-11-30 21:06:29 +0100

    dodata i instancirana klasa StatusBar

---
+++ src/gui/StatusBar.java
@@ -0,0 +1,29 @@
+
+import java.awt.Color;
+import java.text.DateFormat;
+import java.util.Date;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class StatusBar extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public StatusBar() {
+		
+		this.setBackground(Color.GREEN);
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+		JLabel labela = new JLabel ("Studentska Služba");
+		this.add(labela);
+		 this.add(Box.createHorizontalGlue());
+		
+		JLabel labela2 = new JLabel ("vreme");
+		labela2.setText(DateFormat.getDateTimeInstance().format(new Date()));
+		this.add(labela2);		
+		
+	}
+}
+
